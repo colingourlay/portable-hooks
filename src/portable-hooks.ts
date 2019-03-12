96fe51cc61e6b39e6633d47aba60d5ff990e172b
Colin Gourlay <colin@colin-gourlay.com>
2019-03-12 18:34:58 +1000

    All of the hooks now have an extra argument for those rare situations where you want to lie about dependencies.

--- src/portable-hooks.ts
+++ src/portable-hooks.ts
@@ -6,6 +6,7 @@ import {
   useMemo as useMemo_React
 } from 'react';
 
+type Deps = any[];
 type Inputs = any[];
 type Ref<T> = { current: T | null } | ((inst: T | null) => any) | null | void;
 type EffectCleaner = () => void;
@@ -13,22 +14,27 @@ type EffectCreator = (...inputs: Inputs) => EffectCleaner | void;
 type MemoCreator = (...inputs: Inputs) => Function;
 type ImperativeHandleCreator<T> = (...inputs: Inputs) => T;
 
-export function useCallback(callback: Function, inputs?: Inputs | null): Function {
-  return useCallback_React(() => callback.apply(null, inputs), inputs);
+export function useCallback(callback: Function, inputs?: Inputs | null, deps?: Deps): Function {
+  return useCallback_React(() => callback.apply(null, inputs), deps || inputs);
 }
 
-export function useEffect(create: EffectCreator, inputs?: Inputs | null): void {
-  return useEffect_React(() => create && create.apply(null, inputs), inputs);
+export function useEffect(create: EffectCreator, inputs?: Inputs | null, deps?: Deps): void {
+  return useEffect_React(() => create && create.apply(null, inputs), deps || inputs);
 }
 
-export function useImperativeHandle<T>(ref: Ref<T>, create: ImperativeHandleCreator<T>, inputs?: Inputs | null): void {
-  return useImperativeHandle_React(ref, () => create.apply(null, inputs), inputs);
+export function useImperativeHandle<T>(
+  ref: Ref<T>,
+  create: ImperativeHandleCreator<T>,
+  inputs?: Inputs | null,
+  deps?: Deps
+): void {
+  return useImperativeHandle_React(ref, () => create.apply(null, inputs), deps || inputs);
 }
 
-export function useLayoutEffect(create: EffectCreator, inputs?: Inputs | null): void {
-  return useLayoutEffect_React(() => create && create.apply(null, inputs), inputs);
+export function useLayoutEffect(create: EffectCreator, inputs?: Inputs | null, deps?: Deps): void {
+  return useLayoutEffect_React(() => create && create.apply(null, inputs), deps || inputs);
 }
 
-export function useMemo(create: MemoCreator, inputs?: Inputs | null): Function {
-  return useMemo_React(() => create.apply(null, inputs), inputs);
+export function useMemo(create: MemoCreator, inputs?: Inputs | null, deps?: Deps): Function {
+  return useMemo_React(() => create.apply(null, inputs), deps || inputs);
 }
